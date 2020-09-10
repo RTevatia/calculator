5a21b1dbecacce047ec43ae1b084dabb2f9108c6
RTevatia <rtevatia2@gmail.com>
2020-09-10 13:01:29 -0500

    Added more functionalities to catch errors

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 from cal_fun import CalFunc
 
-print("Calucalor for two numbers. Press 'q' to quit")
 while True:
+    print("\nCalucalor for two numbers. Press 'q' to quit")
     num_1 = input("Enter first number (or 'q' to quit): ")
     if num_1 == "q":
         break
@@ -9,18 +9,32 @@ while True:
     if num_2 == "q":
         break
 
-    cf = CalFunc(num_1, num_2)
+    # Check for value error.
+    while True:
+        try:
+            cf = CalFunc(num_1, num_2)
+        except ValueError:
+            print("You must have a number!")
+            break
 
-    print("'1' to divide; '2' to mulitply; '3' to add; and '4' to subtract")
-    func = input("Enter: ")
+        print("'1' to divide; '2' to mulitply; '3' to add; and '4' to subtract")
+        func = input("Enter: ")
 
-    if int(func) == 1:
-        print("{} / {} = {}".format(num_1, num_2, cf.divide()))
-    elif int(func) == 2:
-        print("{} * {} = {}".format(num_1, num_2, cf.multiply()))
-    elif int(func) == 3:
-        print("{} + {} = {}".format(num_1, num_2, cf.add()))
-    elif int(func) == 4:
-        print("{} - {} = {}".format(num_1, num_2, cf.subtract()))
-    else:
-        print("Enter a number from 1-4.")
+        # Check if func is integer
+        try:
+            if int(func) > 0 and int(func) < 5:
+                if int(func) == 1:
+                    print("{} / {} = {}".format(num_1, num_2, cf.divide()))
+                elif int(func) == 2:
+                    print("{} * {} = {}".format(num_1, num_2, cf.multiply()))
+                elif int(func) == 3:
+                    print("{} + {} = {}".format(num_1, num_2, cf.add()))
+                elif int(func) == 4:
+                    print("{} - {} = {}".format(num_1, num_2, cf.subtract()))
+            else:
+                print("Please enter only 1-4")
+        except ValueError:
+            print("Please enter only 1-4")
+
+        # Leave the second while loop
+        break
